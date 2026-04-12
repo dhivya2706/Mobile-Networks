6d40eead8474f1a1ce96066e4e27391adbd9c1de
Dhivya <dhivyaprabha2706@gmail.com>
2026-04-12 17:42:20 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.browse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnDial, btnBrowser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnDial = findViewById(R.id.btnDial);
+        btnBrowser = findViewById(R.id.btnBrowser);
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+                dialIntent.setData(Uri.parse("tel:8270606797"));
+                startActivity(dialIntent);
+            }
+        });
+
+        btnBrowser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+                browserIntent.setData(Uri.parse("https://www.google.com"));
+                startActivity(browserIntent);
+            }
+        });
+    }
+}
